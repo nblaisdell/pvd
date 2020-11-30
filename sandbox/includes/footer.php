d993b696d7b65328309ed41c28f846808aee9ba1
Nathan Blaisdell <nblaisdell@gmail.com>
2020-11-29 22:00:11 -0600

    sandbox draft

---
+++ sandbox/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <div class="block5">
+      <h3>Interested in working together?</h3>
+      <a href="mailto:nblaisdell@gmail.com"><h1>CLICK HERE!</h1></a>
+  </div>
+  <p>Made with ♥ in Dallas</p>
+</footer></body></html>
